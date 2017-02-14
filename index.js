586db9caa77ddd87696a55acbf2239660962de0b
Nick The Sick <nperez0111@gmail.com>
2017-02-13 19:30:00 -0800

    added ways to run the diff functions

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
 #!/usr/bin/env node
 
-const run = a => {
-    var cp = require( 'child_process' );
-    var child = cp.spawn( 'node', [ ( process.argv[ 2 ] || a ) + '.js' ], { detached: true, stdio: [ 'ignore', 'ignore', 'ignore' ] } );
-    child.unref();
-}
-if ( !module.parent ) {
-    run( 'hud' )
-}
-module.exports = {
-    likeSong: () =>
-        run( 'likeSong' ),
-    dislikeSong: () =>
-        run( 'dislikeSong' ),
-    nextSong: () =>
-        run( 'nextSong' ),
-    quit: () =>
-        run( 'quitPianobar' ),
-    getStations: () =>
-        run( 'selectStations' ),
-    playPause: () =>
-        run( 'playPause' ),
-    display: () =>
-        run( 'hud' ),
-    settings: () => {
-        const settings = require( './settings' )
-        settings()
+const commandLineCommands = require( 'command-line-commands' ),
+    getUsage = require( 'command-line-usage' ),
+    usage = getUsage( [ {
+        header: 'Pianobar-Notifier',
+        content: 'Allows you to interact with Pianobar through notifications and this CLI'
+    }, {
+        header: 'Command List',
+        content: [
+            { name: '[bold]{No Params}', summary: 'Displays the current song being played.' },
+            { name: '[bold]{help}', summary: 'Display help information about this app.' },
+            { name: '[bold]{likeSong}', summary: 'Likes the current song being played.' },
+            { name: '[bold]{dislikeSong}', summary: 'Dislikes the current song being played.' },
+            { name: '[bold]{nextSong}', summary: 'Plays the next song on Pianobar' },
+            { name: '[bold]{selectStations}', summary: 'Brings up notification to change the current station to the one you input.' },
+            { name: '[bold]{clearPlaying}', summary: 'If the now playing keeps saying it is paused run this and it will reset.' },
+            { name: '[bold]{playPause}', summary: 'Plays or Pauses the current song.' },
+            { name: '[bold]{settings}', summary: 'Allows you to change some settings of the notifications.' },
+            { name: '[bold]{quit}', summary: 'Quits Pianobar' }
+        ]
+    } ] ),
+    obj = {
+        likeSong: () =>
+            run( 'likeSong' ),
+        dislikeSong: () =>
+            run( 'dislikeSong' ),
+        nextSong: () =>
+            run( 'nextSong' ),
+        quit: () =>
+            run( 'quitPianobar' ),
+        selectStations: () =>
+            run( 'selectStations' ),
+        playPause: () =>
+            run( 'playPause' ),
+        display: () =>
+            run( 'hud' ),
+        settings: () => {
+            const settings = require( './settings' )
+            settings()
+        },
+        clearPlaying: () => {
+            const playPause = require( './playPause' )
+            playPause.clear()
+        },
+        help: () => {
+            console.log( usage )
+        }
     },
-    clearPlaying: () => {
-        const playPause = require( './playPause' )
-        playPause.clear()
+    validCommands = [ null ].concat( Object.keys( obj ) ),
+    { command, argv } = commandLineCommands( validCommands ),
+    run = a => {
+        var cp = require( 'child_process' );
+        var child = cp.spawn( 'node', [ ( process.argv[ 2 ] || a ) + '.js' ], { detached: true, stdio: [ 'ignore', 'ignore', 'ignore' ] } );
+        child.unref();
+    }
+
+if ( argv.includes( '-h' ) || argv.includes( '--help' ) || command == 'help' ) {
+
+    console.log( usage )
+
+} else {
+    if ( !module.parent && command == null ) {
+        run( 'hud' )
+    } else {
+        obj[ command ]()
     }
 }
+
+module.exports = obj
